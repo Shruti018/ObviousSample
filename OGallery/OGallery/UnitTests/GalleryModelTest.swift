4b0fe17d8ac49f759e6158da4608ff82f41fa82b
Shruti <36782850+Shruti018@users.noreply.github.com>
2022-11-15 12:47:16 +0530

    unit test integration

---
+++ OGallery/OGallery/UnitTests/GalleryModelTest.swift
@@ -0,0 +1,43 @@
+//
+//  GalleryModelTest.swift
+//  OGalleryTests
+//
+//  Created by Shruti on 15/11/22.
+//
+
+import XCTest
+@testable import OGallery
+
+class GalleryModelTest: XCTestCase {
+    
+    var viewModel: GalleryViewModel!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.viewModel =  GalleryViewModel()
+        self.viewModel.bindGalleryViewModelToController = {
+        }
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.viewModel = nil
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
